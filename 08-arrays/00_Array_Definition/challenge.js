b4d7245ba3c1b6abf0ecd726839105b5cf2b93df
Gemma <gemmahaylett@gmail.com>
2014-09-02 20:01:36 -0700

    Created functions required by exerciese

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,11 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(num1, num2, num3, num4){
+  var numbers = [num1, num2, num3, num4];
+  return numbers;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array){
+  var firstLastArray = [];
+  firstLastArray.push(array[0]);
+  firstLastArray.push(array[array.length-1]);
+  return firstLastArray;
+};
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,12 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, array){
 
-module.exports.reverseSortedList = undefined;
+  if(array.indexOf(item) < 0){
+    array.push(item);
+  }
+
+  return array;
+};
+
+module.exports.reverseSortedList = function(array){
+  return array.sort().reverse();
+};
