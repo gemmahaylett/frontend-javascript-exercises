bd2465c33fa70aaa12c4517130c360505d6e5342
Gemma <gemmahaylett@gmail.com>
2014-09-08 19:33:04 -0700

    added object exercise answers

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,36 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+  var course = {
+    title : courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
 
-module.exports.addProperty = undefined;
+  return course;
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(myObj, newProp, newValue){
 
-module.exports.canIGet = undefined;
+  if(myObj[newProp] === undefined){
+    myObj[newProp] = newValue;
+    return myObj;
+  }
+  else{
+    return myObj;
+  }
+};
+
+module.exports.formLetter = function(letter){
+  var text = "Hello " + letter.recipient +',\n\n' + letter.msg + '\n\nSincerely,\n' + letter.sender;
+  return text;
+};
+
+module.exports.canIGet = function(item, money){
+  var store = {
+    "MacBook Air" : 999 ,
+    "MacBook Pro" : 1299 ,
+    "Mac Pro" : 2499 ,
+    "Apple Sticker" : 1
+  };
+
+  return store[item] <= money;
+};
